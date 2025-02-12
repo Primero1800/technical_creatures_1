5184c9aa20a635a7ddf60ba71fcdf39c1f635462
Primero1800 <Primero@inbox.ru>
2025-02-12 15:35:43 +0300

    creature

---
+++ src/fake/creature.py
@@ -0,0 +1,74 @@
+from src.model.creature import Creature
+
+_creatures = [
+    Creature(
+        name="Yeti",
+        aka="Abominable Snowman",
+        country="CN",
+        area="Himalayas",
+        description="Hirsute Himalayan"),
+    Creature(
+        name="Bigfoot",
+        description="Yeti's Cousin Eddie",
+        country="US",
+        area="*",
+        aka="Sasquatch"),
+    Creature(
+        name="Grandma Yaga",
+        aka="Bone Leg",
+        country="RU",
+        area="Slavic Woods",
+        description="Eats children"),
+    Creature(
+        name="Koshchey the Immortal",
+        description="Old immortal count, rich, has own death inside egg",
+        country="RU",
+        area="*",
+        aka="Immortal Count"),
+    Creature(
+        name="Zmey Gorynychi",
+        aka="Zmey poganyi",
+        country="RU",
+        area="Golden Hord",
+        description="Has three heads, breathes with fire"),
+    Creature(
+        name="Solovey the Rogue",
+        description="Whist very load",
+        country="RU",
+        area="the ukraine",
+        aka="Morda Blackassed"),
+]
+
+
+def get_all() -> list[Creature]:
+    """Возврат всех существ"""
+    return _creatures
+
+
+def get_one(name: str) -> Creature | None:
+    """Возврат одного существа"""
+    for _creature in _creatures:
+        if _creature.name == name:
+            return _creature
+    return None
+
+
+def create(creature: Creature) -> Creature:
+    """Добавление существа"""
+    return creature
+
+
+def modify(creature: Creature) -> Creature:
+    """Частичное изменение записи существа"""
+    return creature
+
+
+def replace(creature: Creature) -> Creature:
+    """Полная замена записи существа"""
+    return creature
+
+
+def delete(name: str):
+    """Удаление записи существа; возврат значения None,
+    если запись существовала"""
+    return None
---
+++ src/fake/explorer.py
@@ -0,0 +1,48 @@
+from src.model.explorer import Explorer
+
+_explorers = [
+    Explorer(name="Claude Hande",
+        country="FR",
+        description="Scarce during full moons"),
+    Explorer(name="Noah Weiser",
+        country="DE",
+        description="Myopic machete man"),
+    Explorer(name="Ivan Susanin",
+             country="RU",
+             description="Best worker ever"),
+    Explorer(name="Ded Mazai",
+             country="RU",
+             description="Best friend of hears and other animals"),
+]
+
+
+def get_all() -> list[Explorer]:
+    return _explorers
+
+
+def get_one(name: str) -> Explorer | None:
+    for _explorer in _explorers:
+        if _explorer.name in name or name in _explorer.name:
+            return _explorer
+    return None
+
+
+def create(explorer: Explorer) -> Explorer:
+    """Добавление исследователя"""
+    return explorer
+
+
+def modify(explorer: Explorer) -> Explorer:
+    """Частичное изменение записи исследователя"""
+    return explorer
+
+
+def replace(explorer: Explorer) -> Explorer:
+    """Полная замена записи исследователя"""
+    return explorer
+
+
+def delete(name: str) -> bool | None:
+    """Удаление записи исследователя; возврат значения None,
+    если запись существовала"""
+    return None
