824a3a58da7754d68cf1bbdfa94d7b4319eba820
Primero1800 <Primero@inbox.ru>
2025-02-12 13:48:30 +0300

    fastapi

--- project/src/main.py
+++ project/src/main.py
@@ -1,1 +1,19 @@
 from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def top():
+    return 'top here'
+
+
+@app.get("/echo/{thing}")
+def echo(thing):
+    return ' '.join([thing for _ in range(3)])
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
