d39ae4445c3da9d3d6b5d4c5073f3cef308f40ca
Primero1800 <Primero@inbox.ru>
2025-02-13 21:44:36 +0300

    creature cleaned

--- src/web/creature.py
+++ src/web/creature.py
@@ -7,31 +7,37 @@ import src.service.creature as service
 router = APIRouter(prefix='/creature')
 
 
+@router.get("")
 @router.get("/")
 def get_all() -> list[Creature]:
     return service.get_all()
 
 
 @router.get("/{name}")
+@router.get("/{name}/")
 def get_one(name) -> Creature | None:
     return service.get_one(name)
 
 
+@router.post("")
 @router.post("/")
 def create(creature: Creature) -> Creature:
     return service.create(creature)
 
 
+@router.patch("")
 @router.patch("/")
 def modify(creature: Creature) -> Creature:
     return service.modify(creature)
 
 
+@router.put("")
 @router.put("/")
 def replace(creature: Creature) -> Creature:
     return service.replace(creature)
 
 
 @router.delete("/{name}")
+@router.delete("/{name}/")
 def delete(name: str):
     return None
