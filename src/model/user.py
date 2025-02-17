f11169c003909dcf62dee49d077196f664bc06ac
Primero1800 <Primero@inbox.ru>
2025-02-17 17:41:15 +0300

    user added

--- src/model/user.py
+++ src/model/user.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field, SecretStr
 
 class User(BaseModel):
     name: Annotated[str, Field(min_length=2, default='username')]
-    hash: Annotated[SecretStr, Field(min_length=8, )]
+    hash: Annotated[str, Field(min_length=8, )]
 
 
 class UserUpdate(BaseModel):
